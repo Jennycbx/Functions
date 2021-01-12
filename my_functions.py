4aac94e80068023c09e19d0eee32da42486e4ce6
Jenny Bulford <jennycbx@hotmail.com>
2021-01-12 20:35:24 +0000

    Added a second parameter

--- my_functions.py
+++ my_functions.py
@@ -5,8 +5,14 @@
 
 # print(greeting)
 
-def greet(name):
-    return "Hey " + name
+# def greet(name):
+#     return "Hey " + name
 
-greeting = greet("Bob")
+# greeting = greet("Bob")
+# print(greeting)
+
+def greet(name, time_of_day):
+    return "Good " + time_of_day +", " + name
+
+greeting = greet('Bob', 'morning')
 print(greeting)
