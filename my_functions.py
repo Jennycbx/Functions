954822f6e62e974e584e2f3486bf75932637d263
Jenny Bulford <jennycbx@hotmail.com>
2021-01-12 20:47:03 +0000

    Added the laundry function

--- my_functions.py
+++ my_functions.py
@@ -11,8 +11,15 @@
 # greeting = greet("Bob")
 # print(greeting)
 
-def greet(name, time_of_day):
-    return "Good " + time_of_day +", " + name
+# def greet(name, time_of_day):
+#     return "Good " + time_of_day +", " + name
 
-greeting = greet('Bob', 'morning')
-print(greeting)
+# greeting = greet('Bob', 'morning')
+# print(greeting)
+
+def wash_clothes(laundry, detergent, water):
+    clean_laundry = laundry + detergent + water
+    return clean_laundry
+
+clean_tshirt = wash_clothes('dirty_tshirt', 'liquid_detergent', 'hot_water')
+print(clean_tshirt)
