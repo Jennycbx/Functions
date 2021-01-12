2144a89d3fa8a7ee67b37bbe6911c29bf9e55873
Jenny Bulford <jennycbx@hotmail.com>
2021-01-12 20:55:47 +0000

    Added variables as arguments

--- my_functions.py
+++ my_functions.py
@@ -17,9 +17,22 @@
 # greeting = greet('Bob', 'morning')
 # print(greeting)
 
-def wash_clothes(laundry, detergent, water):
-    clean_laundry = laundry + detergent + water
-    return clean_laundry
+# def wash_clothes(laundry, detergent, water):
+#     clean_laundry = laundry + detergent + water
+#     return clean_laundry
 
-clean_tshirt = wash_clothes('dirty_tshirt', 'liquid_detergent', 'hot_water')
-print(clean_tshirt)
+# clean_tshirt = wash_clothes('dirty_tshirt', 'liquid_detergent', 'hot_water')
+# print(clean_tshirt)
+
+def greet(name, time_of_day):
+    return "Good " + time_of_day +", " + name
+
+name_1 = "Bob"
+time_of_day_1 = "morning"
+greeting = greet(name_1, time_of_day_1)
+print(greeting)
+
+name_2 = "Ada"
+time_of_day_2 = "afternoon"
+greeting = greet(name_2, time_of_day_2)
+print(greeting)
